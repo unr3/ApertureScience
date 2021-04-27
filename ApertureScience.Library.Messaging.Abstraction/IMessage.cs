6193a4a9ec175a49c1d4fe87a94e1923978bc928
gurkanvolkansacu <gurkanvolkansacu@gmail.com>
2021-04-27 10:04:17 +0200

    Abstraction of messaging
    IMessage for queue message
    IQueueManager  for send, receive and peek queue messages

---
+++ ApertureScience.Library.Messaging.Abstraction/IMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApertureScience.Library.Messaging.Abstraction
+{
+   public interface IMessage
+    {
+        string MessageId { get; }
+        object Body { get;  }
+        string MessageType { get; }
+        TBody BodyAs<TBody>();
+    }
+}
---
+++ ApertureScience.Library.Messaging.Abstraction/IQueueManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApertureScience.Library.Messaging.Abstraction
+{
+    public interface IQueueManager
+    {
+
+        void CreateClient(string connectionString, string queueName);
+        Task<bool> ExistsAsync();
+        Task SendAsync(IMessage message);
+        Task<IMessage> ReceiveAsync();
+        Task<bool> PeekAsync(string messageId);
+    }
+}
