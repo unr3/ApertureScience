e8674e6fe33556e42a5503b44ce9a72123653980
gurkanvolkansacu <gurkanvolkansacu@gmail.com>
2021-04-27 10:26:41 +0200

    Abtraction of event
    IEvent for event
    IEventDispatcher for dispatch event

---
+++ ApertureScience.Library.Event.Abstraction/IEvent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApertureScience.Library.Event.Abstraction
+{
+   public interface IEvent
+    {
+        string Id { get;}
+        string Name { get; }
+        DateTime DateTimeUTC { get; }
+        
+
+    }
+}
---
+++ ApertureScience.Library.Event.Abstraction/IEventDispatcher.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApertureScience.Library.Event.Abstraction
+{
+   public interface IEventDispatcher
+    {
+        string QueueConnection { get; }
+        string QueueName { get; }
+        void SetQueueConnectionAndName(string queueConnection, string queueName);
+        Task<string> Dispatch(IEvent @event);
+    }
+}
