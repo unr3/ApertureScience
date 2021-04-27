a4c95497959f6d55335488fb0988f76bc55238d5
gurkanvolkansacu <gurkanvolkansacu@gmail.com>
2021-04-27 10:32:34 +0200

    Extension methods
    JsonConvert for object, stream and string

---
+++ ApertureScience.Library.Extension/ObjectExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.IO;
+using System.Text;
+
+namespace ApertureScience.Library.Extension
+{
+    public static class ObjectExtensions
+    {
+        public static string GetMessageType(this object obj)
+        {
+            return obj.GetType().AssemblyQualifiedName;
+        }
+
+        public static string ToJsonString(this object obj)
+        {
+            return JsonConvert.SerializeObject(obj);
+        }
+
+        public static Stream ToJsonStream(this object obj)
+        {
+            var json = obj.ToJsonString();
+            return new MemoryStream(Encoding.Default.GetBytes(json));
+        }
+    }
+}
---
+++ ApertureScience.Library.Extension/StringExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.IO;
+using System.Text;
+
+namespace ApertureScience.Library.Extension
+{
+    public static class StringExtensions
+    {
+
+        public static object ReadFromJson(this string json, string messageType)
+        {
+            var type = Type.GetType(messageType);
+            return JsonConvert.DeserializeObject(json, type);
+        }
+
+        public static T ReadFromJson<T>(this string json)
+        {
+            return JsonConvert.DeserializeObject<T>(json);
+        }
+    }
+}
