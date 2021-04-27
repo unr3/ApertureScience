bea57252e23bd92c3cfaed3e2bd2ef30ebad31d1
gurkanvolkansacu <gurkanvolkansacu@gmail.com>
2021-04-27 10:46:30 +0200

    Implementation of Azure Storage Queue messaging
    BasicMessage : IMessage
    BasicStorageQueueManager : IQueueManager

---
+++ ApertureScience.Library.Messaging.Implementation/BasicMessage.cs
@@ -0,0 +1,60 @@
+﻿
+
+using ApertureScience.Library.Messaging.Abstraction;
+using System;
+using System.IO;
+
+namespace ApertureScience.Library.Messaging.Implementation
+{
+    public class BasicMessage : IMessage
+    {
+
+        private string _messageId;
+        public string MessageId => _messageId;
+
+        private object _body;
+        public object Body => _body;
+
+        private string _messageType;
+        public string MessageType => _messageType;
+
+     
+
+        public TBody BodyAs<TBody>()
+        {
+            return (TBody)Body;
+        }
+       
+        public BasicMessage(string messageId, string messageType, object body)
+        {
+            _messageId = messageId;
+            _messageType = messageType;
+            _body = body;
+
+        }
+
+
+
+
+
+        //public static BasicMessage FromJson(Stream jsonStream)
+        //{
+        //    var message = jsonStream.ReadFromJson<BasicMessage>();
+        //    //the body is a JObject at this point - deserialize to the real message type:
+        //    message.SetBodyAndMessageType(message.Body.ToString().ReadFromJson(message.MessageType));
+        //    return message;
+        //}
+
+        //public static BasicMessage FromJson(string json)
+        //{
+
+
+        //    var message = json.ReadFromJson<BasicMessage>();
+        //    //the body is a JObject at this point - deserialize to the real message type:
+        //    message.SetBodyAndMessageType(message.Body.ToString().ReadFromJson(message.MessageType));
+        //    return message;
+        //}
+
+
+    }
+}
---
+++ ApertureScience.Library.Messaging.Implementation/BasicStorageQueueManager.cs
@@ -0,0 +1,98 @@
+﻿
+using ApertureScience.Library.Messaging.Abstraction;
+using Azure.Storage.Queues;
+using Azure.Storage.Queues.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApertureScience.Library.Messaging.Implementation
+{
+    public class BasicStorageQueueManager : IQueueManager
+    {
+        private  QueueClient _queueClient=null;
+
+        public void CreateClient(string connectionString, string queueName)
+        {
+            _queueClient = new QueueClient(connectionString, queueName, new QueueClientOptions
+            {
+                MessageEncoding = QueueMessageEncoding.Base64
+            });
+        }
+
+
+        public async Task<bool> ExistsAsync()
+        {
+            return await _queueClient.ExistsAsync();            
+        }
+
+        public async Task<bool> PeekAsync(string messageId)
+        {
+            bool result = false;
+            if (await ExistsAsync())
+            {
+                var peeked = await _queueClient.PeekMessageAsync();
+
+                IMessage message = JsonConvert.DeserializeObject<BasicMessage>(peeked.Value.Body.ToString());
+              
+                
+                result= message.MessageId == messageId;
+            }
+            else
+            {
+                throw new Exception("Queue is not found");
+            }
+            return result;
+        }
+
+        public async Task<IMessage> ReceiveAsync()
+        {
+           
+            if(await ExistsAsync())
+            {
+
+                QueueProperties properties = await _queueClient.GetPropertiesAsync();
+
+                if (properties.ApproximateMessagesCount > 0)
+                {
+                    var message
+                        = await _queueClient.ReceiveMessageAsync();
+                    return  JsonConvert.DeserializeObject<BasicMessage>(message.Value.Body.ToString()); ;
+                }
+                else
+                {
+                    throw new Exception("Message is not found");
+                }
+
+            }
+            else
+            {
+                throw new Exception("Queue is not found");
+            }
+
+            
+           
+            
+           
+        }
+
+        public async Task SendAsync(IMessage message)
+        {
+           
+            
+            if (await ExistsAsync())
+            {
+                string json = JsonConvert.SerializeObject(message) ;
+                // Send a message to the queue
+               await _queueClient.SendMessageAsync(json);
+                
+            }
+            else
+            {
+                throw new Exception("Queue is not found");
+            }
+        }
+    }
+}
