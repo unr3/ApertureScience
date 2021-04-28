837098b682cb2dd24821910d958a7494522af10d
gurkanvolkansacu <gurkanvolkansacu@gmail.com>
2021-04-28 20:00:51 +0200

    GetActivationBatchCode action added in to the ActivationCode controller
    Similar to GetAvtivationCode action. Instead of returning the "ActivationCodeRequestEvent" result
    it gives to "ActivationCodeBatchRequestEvent" result.
    
    if it finds the message it returns result of request. Otherwise it returns 404 not found

--- ApertureScience.Web.ApiGateway/Controllers/ActivationCode.cs
+++ ApertureScience.Web.ApiGateway/Controllers/ActivationCode.cs
@@ -77,6 +77,42 @@ namespace ApertureScience.Web.ApiGateway.Controllers
                 return NotFound();
 
         }
+        /// <summary>
+        /// Get result from requested generatecode batch action 
+        /// </summary>
+        /// <param name="messageId">Id for requested event message</param>
+        /// <returns></returns>
+        [HttpGet()]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> GetActivationBatchCode(string messageId)
+        {
+            if (string.IsNullOrEmpty(messageId))
+                return BadRequest("Message Id is empty");
+
+            Guid requestedMessageId;
+            if (!Guid.TryParse(messageId, out requestedMessageId))
+                return BadRequest("Message Id is not valid");
+
+            var queueInfo = _queueInfoService.GetQueueInfo(nameof(ActivationCodeBatchRespondedEvent));
+            if (queueInfo == null)
+                throw new ArgumentNullException(nameof(queueInfo));
+
+            _queueManager.CreateClient(queueInfo.QueueConnection, queueInfo.QueueName);
+
+            bool messageFound = await _queueManager.PeekAsync(messageId);
+
+            if (messageFound)
+            {
+                IMessage receivedMessage = await _queueManager.ReceiveAsync();
+                var respondedEvent = receivedMessage.Body.ToString().ReadFromJson<ActivationCodeBatchRespondedEvent>();
+                return Ok(respondedEvent.Result.ToString());
+            }
+            else
+                return NotFound();
+
+        }
 
         [HttpPost]
         [Consumes("application/json")]
