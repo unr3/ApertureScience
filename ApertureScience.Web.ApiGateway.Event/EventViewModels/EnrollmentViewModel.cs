527cd934f470189031e44586adbcea955290c930
gurkanvolkansacu <gurkanvolkansacu@gmail.com>
2021-04-29 10:30:54 +0200

    EnrollmentViewModel added to ApiGateway ViewModels
    Enrollment function use this viewmodel to carry data between api gateway and enrollment function

---
+++ ApertureScience.Web.ApiGateway.Event/EventViewModels/EnrollmentViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ApertureScience.Web.ApiGateway.Event.ViewModels
+{
+  public class EnrollmentViewModel
+    {
+        [Required]
+        public string FullName { get; set; }
+        [EmailAddress]
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public int Code { get; set; }
+
+      
+    }
+}
