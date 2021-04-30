3cc35ad7b4a9d065fb7e18dbb43b0a57bd9139c4
gurkanvolkansacu <gurkanvolkansacu@gmail.com>
2021-04-30 10:57:33 +0200

    IngestData action added in the Accelerometer controller
    Enroll action fires a request event to the relevant message queue.
    
    Steps
    
    - It takes "IngestDataViewModel" parameter.
    - Then it creates "AccelerometerIngestRequestedEvent" and gives it required value(s).In this case the parameters are userId,dimensionX,dimensionY,dimensionZ,timestamp
    - if data is bigger thand "IngestBlockSize" it divides data by block size
    
    - After that it can read the information about the queue connection and queue name from the appsettings file by event name.
    - "IQueueInfoService" helps us to read queue info from appsettings file and returns "QueueInfoViewModel" model.
    - "EventDispatcher" class helps us to send the requested event to the relevant queue and returns "MessageId"
    -  Finally it wraps this "MessageId(s)" with "IngestDataResponseViewModel" type and returns it.
    
     AccelerometerIngestRequestedEvent : derived from IEvent (ApertureScience.Library.Event.Abstraction)
     EventDispatcher                   : derived from IEventDispatcher (ApertureScience.Library.Event.Abstraction)

---
+++ ApertureScience.Web.ApiGateway.Event/EventViewModels/IngestDataViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ApertureScience.Web.ApiGateway.Event.ViewModels
+{
+    public class IngestDataViewModel
+    {
+        [Required]
+        public Guid UserId { get;  set; }
+        [Required]
+        public int DimensionX { get;  set; }
+        [Required]
+        public int DimensionY { get;  set; }
+        [Required]
+        public int DimensionZ { get;  set; }
+        [Required]
+        public long TimeStamp { get;  set; }
+       
+    }
+}
---
+++ ApertureScience.Web.ApiGateway/Controllers/Accelerometer.cs
@@ -0,0 +1,104 @@
+﻿using ApertureScience.Library.Event.Abstraction;
+using ApertureScience.Library.Messaging.Abstraction;
+using ApertureScience.Web.ApiGateway.Event;
+using ApertureScience.Web.ApiGateway.Event.ViewModels;
+using ApertureScience.Web.ApiGateway.Services;
+using ApertureScience.Web.ApiGateway.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApertureScience.Web.ApiGateway.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class Accelerometer : ControllerBase
+    {
+        private readonly IQueueInfoService _queueInfoService;
+        private readonly IEventDispatcher _eventDispatcher;
+        private readonly IQueueManager _queueManager;
+        private readonly IConfiguration _configuration;
+
+        public Accelerometer(IQueueInfoService queueInfoService, IEventDispatcher eventDispatcher, IQueueManager queueManager, IConfiguration configuration)
+        {
+            if (queueInfoService == null)
+                throw new ArgumentNullException(nameof(queueInfoService));
+
+            if (eventDispatcher == null)
+                throw new ArgumentNullException(nameof(eventDispatcher));
+
+            if (queueManager == null)
+                throw new ArgumentNullException(nameof(queueManager));
+
+            if (configuration == null)
+                throw new ArgumentNullException(nameof(configuration));
+
+            _queueInfoService = queueInfoService;
+            _eventDispatcher = eventDispatcher;
+            _queueManager = queueManager;
+            _configuration = configuration;
+        }
+        [HttpPost]
+        [Consumes("application/json")]
+        [ProducesResponseType(StatusCodes.Status202Accepted, Type = typeof(IngestDataResponseViewModel))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [Produces("application/json")]
+        public async Task<ActionResult<IngestDataResponseViewModel>>IngestData(IngestDataViewModel[] data)
+        {
+            if (data == null || data.Length == 0)
+                return BadRequest("Data is empty");
+
+
+            if (!ModelState.IsValid)
+            {
+                var errors = new List<string>();
+                foreach (var state in ModelState)
+                {
+                    foreach (var error in state.Value.Errors)
+                    {
+                        errors.Add(error.ErrorMessage);
+                    }
+                }
+
+                return BadRequest(errors);
+            }
+
+            int ingestBlockSize;
+
+            if (!int.TryParse(_configuration["IngestBlockSize"], out ingestBlockSize) || ingestBlockSize <= 0)
+                throw new ArgumentNullException(nameof(ingestBlockSize));
+
+            var queueInfo = _queueInfoService.GetQueueInfo(nameof(AccelerometerIngestRequestedEvent));
+            if (queueInfo == null)
+                throw new ArgumentNullException(nameof(queueInfo));
+
+            _eventDispatcher.SetQueueConnectionAndName(queueInfo.QueueConnection, queueInfo.QueueName);
+
+            int dataParts = (data.Length + ingestBlockSize - 1) / ingestBlockSize;
+
+            List<string> messageIdList = new List<string>();
+            for (int i = 0; i < dataParts; i++)
+            {
+                var requestedEvent = new AccelerometerIngestRequestedEvent(Guid.NewGuid().ToString(), nameof(AccelerometerIngestRequestedEvent), DateTime.UtcNow, data.Skip(i * ingestBlockSize).Take(ingestBlockSize));
+
+                string messageId = await _eventDispatcher.Dispatch(requestedEvent);
+                messageIdList.Add(messageId);
+            }
+
+            string responseMessageId = null;
+            if (messageIdList.Count == 1)
+                responseMessageId = messageIdList[0];
+            else
+                responseMessageId = String.Join(",", messageIdList);
+
+            var response = new IngestDataResponseViewModel { Success = true, ResponseMessage = "IngestData request accepted.", MessageId = responseMessageId };
+
+            return Accepted(response);
+
+        }
+    }
+}
